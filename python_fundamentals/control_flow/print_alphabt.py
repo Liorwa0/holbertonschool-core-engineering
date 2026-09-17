8d59c479eea75e199a50d8c10efec7483dc5f799
Liorwa0 <lla644183@gmail.com>
2026-09-17 04:30:35 +0200

    Fix print_alphabt.py using .format()

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-output = ""
 for i in range(97, 123):
-    if chr(i) != 'e' and chr(i) != 'q':
-        output += chr(i)
-print(output)
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
